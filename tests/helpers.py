2ba5bf6821a50a2f17af562a4936c3692fc2d6c1
OlegSergeev960 <OlegSergeev960@users.noreply.github.com>
2026-04-29 15:54:28 +0000

    deploy: 13cbd9d08c96239c88d832145f6f5a6e5982bd6b

--- tests/helpers.py
+++
@@ -1,6 +0,0 @@
-import allure
-
-@allure.step('Проверяем статус-код')
-def check_status_code(response, expected_status=200):
-    assert response.status_code == expected_status
-    f'Ожидался статус-код {expected_status}, но получен {response.status_code}'
